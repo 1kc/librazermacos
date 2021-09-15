cc1699a31dfda17434d887b2ad973af451e97935
WrathofthePast <75559873+WrathofthePast@users.noreply.github.com>
2021-09-15 18:41:49 -0400

    Add files via upload

---
+++ librazermacos-master/README.md
@@ -0,0 +1,24 @@
+# librazermacos
+
+C library of openrazer drivers ported to macOS.
+
+Exposed as a shared/dynamic library - `librazermacos.so` to provider interoperability with various frontends.
+
+Includes a sample CLI implementation in C to demonstrate driver function calls.
+
+To build the shared library and sample cli
+```bash
+make
+```
+Running the CLI
+```bash
+./sample_cli
+```
+
+To build the shared library only.
+```bash
+make librazermacos.so
+```
+
+## razer-macos frontend
+[razer-macos](https://github.com/1kc/razer-macos/) does not actually build `librazermacos.so`, instead the drivers are built by [binding.gyp](https://github.com/1kc/razer-macos/blob/master/binding.gyp).
---
+++ librazermacos-master/src/sample_cli.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "razerdevice.h"
+#include "razerkbd_driver.h"
+
+int main(int argc, const char * argv[]) {
+    RazerDevices allDevices = getAllRazerDevices();
+    RazerDevice *razerDevices = allDevices.devices;
+
+    printf("%d Razer device(s) found:\n", allDevices.size);
+
+    for (int i = 0; i < allDevices.size; i++) {
+        RazerDevice device = razerDevices[i];
+        printf("%#06x\n", device.productId);
+
+        // Testing out the blackwidow v3 pro changes by switching to wave
+        if (device.productId == 0x025A)
+        {
+            printf("Found blackwidow v3 pro keyboard (wired)\n");
+            razer_attr_write_mode_wave(device.usbDevice, "1", 0, 0x90);
+        }
+    }
+
+    closeAllRazerDevices(allDevices);
+    return 0;
+
+}
