a0d55638d16c49505ecb123ee098b78ac58d41fd
WrathofthePast <75559873+WrathofthePast@users.noreply.github.com>
2022-02-16 00:06:39 -0500

    Resolve kIOMasterPortDefault Deprecation

--- src/lib/razerdevice.c
+++ src/lib/razerdevice.c
@@ -238,7 +238,7 @@ IOUSBDeviceInterface **getRazerUSBDeviceInterface(int type)
 
 	io_iterator_t iter;
 	kern_return_t kReturn =
-		IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iter);
+		IOServiceGetMatchingServices(kIOMainPortDefault, matchingDict, &iter);
 	if (kReturn != kIOReturnSuccess)
 	{
 		return NULL;
@@ -384,7 +384,7 @@ RazerDevices getAllRazerDevices()
 
     io_iterator_t iter;
     kern_return_t kReturn =
-            IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iter);
+            IOServiceGetMatchingServices(kIOMainPortDefault, matchingDict, &iter);
     if (kReturn != kIOReturnSuccess)
     {
         return allDevices;
