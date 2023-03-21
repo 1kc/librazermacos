6ac2fac06457703d609fb3600583c188ebcca3bc
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2023-03-21 12:20:40 +0000

    Added Razer Laptop Stand
    Tested and confirmed as working.  Added support for the Razer Laptop
    Stand with support for static, spectrum, breating and wave.

--- src/lib/razerdevice.c
+++ src/lib/razerdevice.c
@@ -226,6 +226,7 @@ bool is_accessory(IOUSBDeviceInterface **usb_dev)
         case USB_DEVICE_ID_RAZER_MOUSE_BUNGEE_V3_CHROMA:
         case USB_DEVICE_ID_RAZER_BASE_STATION_V2_CHROMA:
         case USB_DEVICE_ID_RAZER_THUNDERBOLT_4_DOCK_CHROMA:
+        case USB_DEVICE_ID_RAZER_LAPTOP_STAND_CHROMA:
             return true;
     }
 
